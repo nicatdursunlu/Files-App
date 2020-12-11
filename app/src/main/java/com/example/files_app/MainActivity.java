fe31b6d4658b02d190f2af7cb96f9a38eb6751b1
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2020-12-11 22:37:50 +0400

    some changes

--- app/src/main/java/com/example/files_app/MainActivity.java
+++ app/src/main/java/com/example/files_app/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.files_app;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Camera;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +18,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private static final int REQ_CODE_TAKE_PICTURE = 569;
+
+    public void takePhoto(View view) {
+        Intent pictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(pictureIntent, REQ_CODE_TAKE_PICTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQ_CODE_TAKE_PICTURE && resultCode == RESULT_OK) {
+            Bitmap bmp = (Bitmap) data.getExtras().get("data");
+            ImageView img = (ImageView) findViewById(R.id.camera_image);
+            img.setImageBitmap(bmp);
+        }
+    }
+
 }
